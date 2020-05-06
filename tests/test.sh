717471d59edcc7f01bce01c15eec9902561d58f3
David Deng <daviddenghaotian@163.com>
2020-05-06 00:27:10 -0400

    moved test files to tests folder, added a new line at end of ans files

---
+++ tests/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+TARGET_SCRIPT="python3 ../start.py"
+echo "testing on $TARGET_SCRIPT"
+
+for INFILE in *.in; do
+	ROOT=${INFILE%\.in}
+	OUTFILE=${ROOT}.ans
+	eval $TARGET_SCRIPT < ${INFILE} | diff $OUTFILE -
+	ret=$?
+	if [ $ret -eq 0 ]; then
+		echo "PASSED...${ROOT}"
+	elif [ $ret -eq 1 ]; then
+		echo "FAILED...${ROOT}"
+	elif [ $ret -eq 2 ]; then
+		echo "ERROR...${ROOT}"
+	else
+		echo "UNKNOWN ERROR...${ROOT}"
+	fi
+done
